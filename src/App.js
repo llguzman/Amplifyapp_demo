378a9a4a1aac0dec7182077186090e9e93713169
llguzman <79930946+llguzman@users.noreply.github.com>
2021-03-02 12:49:00 -0500

    Update app.js
    Add comments

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Versión 2.
+          Edit <code>src/App.js</code> and save to reload Versión 3 con rama nueva.
         </p>
         <a
           className="App-link"
